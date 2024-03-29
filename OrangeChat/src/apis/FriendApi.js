f1c97f6a63e169e0396ae00a5a851f3c89010290
Nguyễn Sang <sangnn8@gmail.com>
2024-03-29 15:22:18 +0700

    fix friend

--- OrangeChat/src/apis/FriendApi.js
+++ OrangeChat/src/apis/FriendApi.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://192.168.249.29:3000/api/friend';
-
+const BASE_URL = 'http://192.168.1.8:3000/api/friend';
 
 const instance = axios.create({
   baseURL: BASE_URL,
@@ -28,15 +27,40 @@ const getFriendRequests = async ({userId}) => {
   }
 };
 
-const sendFriendRequest = async ({receiverId,senderId}) => {
+const getAllFriendRequests = async()=>{
+   try {
+     const res = await instance.get(`/getAllFriendRequests`);
+     return res.data;
+   } catch (error) {
+     console.log('error', error);
+     throw error;
+   }
+}
+
+const sendFriendRequest = async ({receiverId, senderId}) => {
   try {
     const res = await instance.post('/send', {
       senderId,
       receiverId,
     });
-
   } catch (error) {
     throw error;
   }
 };
-export default {getFriends, getFriendRequests,sendFriendRequest};
+
+const accept = async ({friendRequestId}) => {
+  try {
+    const res = await instance.put(`/${friendRequestId}`);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const reject = async ({friendRequestId}) => {
+  try {
+    const res = await instance.delete(`/${friendRequestId}`);
+  } catch (error) {
+    throw error;
+  }
+};
+export default {getFriends, getFriendRequests, sendFriendRequest, accept,reject,getAllFriendRequests};
--- OrangeChat/src/redux/friendSlice.js
+++ OrangeChat/src/redux/friendSlice.js
@@ -1,4 +1,5 @@
 import {createSlice} from '@reduxjs/toolkit';
+import FriendApi from '../apis/FriendApi';
 
 const friendSlice = createSlice({
   name: 'friend',
@@ -13,8 +14,35 @@ const friendSlice = createSlice({
     setFriendRequests: (state, action) => {
       state.listFriendRequests = action.payload;
     },
+    updateFriendRequests: (state, action) => {
+      state.listFriendRequests = state.listFriendRequests.filter(
+        fq => fq._id !== action.payload,
+      );
+    },
+    addFriendRequsts: (state, action) => {
+      state.listFriendRequests.push(action.payload);
+    },
   },
 });
 
-export const {setFriends, setFriendRequests} = friendSlice.actions;
+export const {setFriends, setFriendRequests, updateFriendRequests,addFriendRequsts} =
+  friendSlice.actions;
+export const fetchFriends = userId => async dispatch => {
+  try {
+    const friends = await FriendApi.getFriends({userId});
+    dispatch(setFriends(friends.data));
+  } catch (error) {
+    console.error('Error fetching friends:', error);
+  }
+};
+
+export const fetchFriendRequests = userId => async dispatch => {
+  try {
+    const friendRequests = await FriendApi.getFriendRequests({userId});
+    dispatch(setFriendRequests(friendRequests.data));
+  } catch (error) {
+    console.error('Error fetching friend requests:', error);
+  }
+};
+
 export default friendSlice.reducer;
